871a61c1322de685c351af136911e58b3134192f
sajeebmahamed <sajeebmahamed@gmail.com>
2021-04-25 11:10:03 +0600

    user endpoint added

---
+++ api/events/controllers/events.js
@@ -0,0 +1,27 @@
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  //get logged in user
+
+  async me(ctx) {
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was not found" }] },
+      ]);
+    }
+    const data = await starpi.services.events.find({ user: user.id });
+
+    if (!data) {
+      return ctx.notFound();
+    }
+    return sanitizeEntity(data, { model: starpi.models.events });
+  },
+};
